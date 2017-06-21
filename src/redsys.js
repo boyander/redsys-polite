5a833779e5696724f6dbe8eb1c1574ff7ba6babe
Marc Pomar <marc@faable.com>
2017-06-21 18:25:55 +0200

    add ava

--- src/redsys.js
+++ src/redsys.js
@@ -2,106 +2,128 @@
 import PaymentBuilder from './payment';
 import crypto from 'crypto';
 
-var Redsys = function(options){
+class Redsys {
+  constructor(parmeters) {
     Object.assign(this, options);
-    this.generateMerchantParams = function(payment){
-        return {
-            "DS_MERCHANT_AMOUNT":payment.total,
-            "DS_MERCHANT_ORDER":payment.order_id,
-            "DS_MERCHANT_MERCHANTCODE": this.merchantCode,
-            "DS_MERCHANT_CURRENCY":payment.currency,
-            "DS_MERCHANT_TRANSACTIONTYPE":this.transaction_type,
-            "DS_MERCHANT_TERMINAL":this.terminal,
-            "DS_MERCHANT_MERCHANTURL":payment.redirect_urls.merchant_url,
-            "DS_MERCHANT_URLOK":payment.redirect_urls.ok_url,
-            "DS_MERCHANT_URLKO":payment.redirect_urls.cancel_url,
-            'DS_MERCHANT_CONSUMERLANGUAGE':'001',
-            'DS_MERCHANT_TITULAR':this.titular,
-            'DS_MERCHANT_MERCHANTNAME':this.name,
+    this.generateMerchantParams = function(payment) {
+      return {
+        "DS_MERCHANT_AMOUNT": payment.total,
+        "DS_MERCHANT_ORDER": payment.order_id,
+        "DS_MERCHANT_MERCHANTCODE": this.merchantCode,
+        "DS_MERCHANT_CURRENCY": payment.currency,
+        "DS_MERCHANT_TRANSACTIONTYPE": this.transaction_type,
+        "DS_MERCHANT_TERMINAL": this.terminal,
+        "DS_MERCHANT_MERCHANTURL": payment.redirect_urls.merchant_url,
+        "DS_MERCHANT_URLOK": payment.redirect_urls.ok_url,
+        "DS_MERCHANT_URLKO": payment.redirect_urls.cancel_url,
+        'DS_MERCHANT_CONSUMERLANGUAGE': '001',
+        'DS_MERCHANT_TITULAR': this.titular,
+        'DS_MERCHANT_MERCHANTNAME': this.name,
 
-            // Test code
-            //"DS_MERCHANT_PAN":"4548812049400004",
-            //"DS_MERCHANT_EXPIRYDATE":"1220",
-            //"DS_MERCHANT_CVV2":"123"
-        };
+        // Test code
+        //"DS_MERCHANT_PAN":"4548812049400004",
+        //"DS_MERCHANT_EXPIRYDATE":"1220",
+        //"DS_MERCHANT_CVV2":"123"
+      };
     };
+  }
 
-    this.encodeOrder = function(order_id, secret){
-        var secretKey = new Buffer(secret, 'base64');
-        var iv = new Buffer(8);
-        iv.fill(0);
-        var cipher = crypto.createCipheriv('des-ede3-cbc', secretKey, iv);
-        cipher.setAutoPadding(false);
-        var res = cipher.update(order_id, 'utf8', 'base64') + cipher.final('base64');
-        return res;
+  encodeOrder(order_id, secret) {
+    var secretKey = new Buffer(secret, 'base64');
+    var iv = new Buffer(8);
+    iv.fill(0);
+    var cipher = crypto.createCipheriv('des-ede3-cbc', secretKey, iv);
+    cipher.setAutoPadding(false);
+    var res = cipher.update(order_id, 'utf8', 'base64') + cipher.final('base64');
+    return res;
+  }
+
+  doSignature(order_encoded, merchantData) {
+    var hexMac256 = crypto.createHmac("sha256", new Buffer(order_encoded, 'base64')).update(merchantData).digest("hex");
+    return new Buffer(hexMac256, 'hex').toString('base64');
+  }
+
+  validateSignature(signature, merchantData){
+    // Se decodifica la clave Base64
+    let key = new Buffer(secret, 'base64');
+    let decodedData = JSON.parse(new Buffer(merchantData,'base64'));
+
+/*
+// Se decodifican los datos Base64
+$decodec = $this->base64_url_decode($datos);
+// Los datos decodificados se pasan al array de datos
+$this->stringToArray($decodec);
+// Se diversifica la clave con el Número de Pedido
+$key = $this->encrypt_3DES($this->getOrderNotif(), $key);
+// MAC256 del parámetro Ds_Parameters que envía Redsys
+$res = $this->mac256($datos, $key);
+// Se codifican los datos Base64
+return $this->base64_url_encode($res);	*/
+    var hexMac256 = crypto.createHmac("sha256", new Buffer(order_encoded, 'base64')).update(merchantData).digest("hex");
+    return new Buffer(hexMac256, 'hex').toString('base64');
+
+  }
+
+  getFormData(payment) {
+    // Merchant parameters as a Base64-JSON
+    var merchant = new Buffer(JSON.stringify(this.generateMerchantParams(payment))).toString('base64');
+
+    // Calculate signature
+    var order_encoded = this.encodeOrder(payment.order_id, this.secret);
+    var signature = this.doSignature(order_encoded, merchant);
+
+    return {
+      'redsys_url': this.url,
+      'Ds_SignatureVersion': 'HMAC_SHA256_V1',
+      'Ds_MerchantParameters': merchant,
+      'Ds_Signature': signature,
     };
+  }
+}
 
-    this.doSignature = function(order_encoded, merchant){
-        var hexMac256 = crypto.createHmac("sha256", new Buffer(order_encoded, 'base64')).update(merchant).digest("hex");
-        return new Buffer(hexMac256, 'hex').toString('base64');
-    };
-
-    this.getFormData = function(payment){
-        // Merchant parameters as a Base64-JSON
-        var merchant = new Buffer(JSON.stringify(this.generateMerchantParams(payment))).toString('base64');
-
-        // Calculate signature
-        var order_encoded = this.encodeOrder(payment.order_id, this.secret);
-        var signature = this.doSignature(order_encoded, merchant);
-
-        return {
-            'redsys_url': this.url,
-            'Ds_SignatureVersion':'HMAC_SHA256_V1',
-            'Ds_MerchantParameters': merchant,
-            'Ds_Signature': signature,
-        };
-    };
-};
-
-
-class RedsysBuilder{
-  constructor(){
+class RedsysBuilder {
+  constructor() {
     this.name = "Default-Redsys";
     this.terminal = "1";
     this.language = "auto";
-    this.transaction_type="0";
+    this.transaction_type = "0";
     // Production URL
     this.url = "https://sis.redsys.es/sis/realizarPago";
   }
-  setMerchantCode(merchant_code){
-      this.merchantCode = merchant_code;
-      return this;
+  setMerchantCode(merchant_code) {
+    this.merchantCode = merchant_code;
+    return this;
   }
-  setTerminal(terminal_number){
-      this.terminal = terminal_numbver;
-      return this;
+  setTerminal(terminal_number) {
+    this.terminal = terminal_numbver;
+    return this;
   }
-  setName(name){
-      this.name = name;
-      return this;
+  setName(name) {
+    this.name = name;
+    return this;
   }
-  setTitular(titular){
-      this.titular = titular;
-      return this;
+  setTitular(titular) {
+    this.titular = titular;
+    return this;
   }
-  setSecret(secret){
-      this.secret = secret;
-      return this;
+  setSecret(secret) {
+    this.secret = secret;
+    return this;
   }
-  enableDebug(){
-      // Change to debug url
-      this.url = "https://sis-t.redsys.es:25443/sis/realizarPago";
-      return this;
+  enableDebug() {
+    // Change to debug url
+    this.url = "https://sis-t.redsys.es:25443/sis/realizarPago";
+    return this;
   }
-  build(){
-      if(this.merchantCode === undefined) throw new Error("Merchant Code not set");
-      if(this.titular === undefined) throw new Error("Titular not set");
-      if(this.secret === undefined) throw new Error("Secret not set");
-      return new Redsys(this);
+  build() {
+    if (this.merchantCode === undefined) throw new Error("Merchant Code not set");
+    if (this.titular === undefined) throw new Error("Titular not set");
+    if (this.secret === undefined) throw new Error("Secret not set");
+    return new Redsys(this);
   }
 }
 
 export {
-    PaymentBuilder,
-    RedsysBuilder
+  PaymentBuilder,
+  RedsysBuilder
 };
---
+++ test/test.spec.js
@@ -0,0 +1,63 @@
+/*jshint esversion:6*/
+
+import test from 'ava';
+import {RedsysBuilder, PaymentBuilder} from '../lib/redsys';
+
+const secret_data = {
+  commerce_code: process.env.COMMERCE_CODE || '000000000',
+  secret_code: process.env.SECRET_CODE || "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
+console.log(secret_data);
+
+test('builder works with all required properties set',t => {
+  var redsys = new RedsysBuilder()
+              .setMerchantCode(secret_data.commerce_code)
+              .setTitular("Marc Pomar")
+              .setSecret(secret_data.secret_code)
+              .build();
+  t.is(redsys.secret, secret_data.secret_code);
+  t.not(redsys.url,undefined);
+});
+
+test('throws error if missing properties on builder',t => {
+  var builder = new RedsysBuilder()
+      .setMerchantCode("33")
+      .setTitular("Marc Pomar");
+  t.throws(() => builder.build());
+});
+
+test('throws error if missing properties on builder',t => {
+  var paymentBuilder = new PaymentBuilder()
+      .setTotal(3.20)
+      .setOrderId("123456")
+      .setUrlMerchant("http://faable.com")
+      .setUrlCancel("http://faable.com/cancel")
+      .setUrlOK("http://faable.com/accept");
+  t.notThrows(() => paymentBuilder.build());
+});
+
+
+test('throws error if missing properties on builder',t => {
+  const redsys = new RedsysBuilder()
+      .setMerchantCode(secret_data.commerce_code)
+      .setName("Faable.com")
+      .setTitular("Marc Pomar")
+      .setSecret(secret_data.secret_code)
+      .enableDebug()
+      .build();
+
+  const payment = new PaymentBuilder()
+      .setTotal(3.20)
+      .setOrderId("1")
+      .setUrlMerchant("http://faable.com")
+      .setUrlCancel("http://faable.com/cancel")
+      .setUrlOK("http://faable.com/accept")
+      .build();
+
+  const form_encoded_params = redsys.getFormData(payment);
+  const all_params = redsys.generateMerchantParams(payment);
+  t.not(form_encoded_params, undefined);
+  t.not(all_params, undefined);
+  t.snapshot(form_encoded_params);
+  t.snapshot(all_params);
+});
