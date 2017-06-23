bdd1c484da79293a01ac671f0d42a2a5ef2a1919
Marc Pomar <marc@faable.com>
2017-06-23 16:18:09 +0200

    Publish changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
+var PACKAGE = require('./package.json');
+var banner = PACKAGE.name + ' - ' + PACKAGE.version;
 
 module.exports = {
   entry: './src/index.js',
+  target: 'node',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'redsys-polite.js',
-    library: "redsys-polite"
-  },
-  externals : {
-    crypto: 'crypto'
+    libraryTarget:"umd"
   },
   module: {
     rules: [{
       test: /\.js$/,
-      exclude: /node_modules/,
       loader: 'babel-loader'
     }]
   },
   plugins:[
     new webpack.BannerPlugin({
-      banner: " Marc Pomar Torres - marc@faable.com \n created by Faable.com \n file:[file] "
+      banner: ` ${banner}\n Marc Pomar Torres - marc@faable.com \n created by Faable.com \n file:[file] `
     })
   ]
 };
